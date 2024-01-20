a8b3598798e62a558663e95ac96d8d676b2202d4
nosepato <felipe.pinhoramos@outlook.com>
2024-01-20 11:05:48 -0300

    adicionando novos arquivos

---
+++ src/renderer/src/components/Dices/Dices.jsx
@@ -0,0 +1,13 @@
+import { useState, useRef } from 'react'
+
+import './index.css'
+
+function Dices() {
+    const [result, setResult] = useState()
+    const [history, setHistory] = useState([])
+    return (
+    <div>Dices</div>
+  )
+}
+
+export default Dices
---
+++ src/renderer/src/components/Form.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+// function Form (props, hasButton) {
+//   hasButton = true  
+//   return (
+//     <div>
+//         <form action="">
+//             <label>{props.label1}</label>
+//             <input type="text" placeholder='Felipe' />
+//             <label>{props.label2}</label>
+//             <input type="text" placeholder='25' />
+//             {hasButton && <button type='submit'>{props.textButton}</button>}
+//         </form>
+//   </div>
+//   )
+// }
+
+// export default Form
